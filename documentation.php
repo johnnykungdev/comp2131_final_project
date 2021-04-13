7b55a4ab10f55d9a9aa4fd311528e7f5f89d27b9
Johnny Kung <kjohnathan.engi@gmail.com>
2021-04-13 00:46:47 -0700

    added source page

--- documentation.php
+++ documentation.php
@@ -21,41 +21,20 @@
   <div class="box card-panel z-depth-3">
     <div class="merchant">
       <img style="display: block, margin: 0 auto" src="https://api.freelogodesign.org/files/1108d377a5a74180b8f5f7276901361f/thumb/logo_200x200.png?v=637537258370000000" />
-      <h5>Sources</h5>
+      <h5>Documentation</h5>
       
     </div>
     <div class="invoice">
     
-    <h2>Images</h2>
-    <p>
-    1. Westcoastfood.ca. Published 2021. Accessed April 1, 2021. 
-    https://westcoastfood.ca/wp-content/uploads/2017/05/Szechuan-Delicious-Dan-Dan-noodles.jpg
-    <br>
-    <br>
-    2. Theseasonedmom.com. Published 2021. Accessed March 28, 2021.
-    https://www.theseasonedmom.com/wp-content/uploads/2018/02/The-Easiest-Burrito-Recipe-7.jpg
-‌    <br>
-    <br>
-    <h2>CSS Framework</h2>
-    <p>
-‌    3. Materializecss.com Published 2014. Accessed April 02, 2021.
-    <span>https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css</span>
-    <br>
-    <br>
-    4. Materializecss.com Published 2014-2015. Accessed April 02, 2021.
-    https://fonts.googleapis.com/icon?family=Material+Icons
-    <br>
-    <br>
-    5. CodePen - Gradient Background 2021, Alison Quaglia . Accessed April 02, 2021.
-    https://codepen.io/hylobates-lar/pen/qBbQeON
+    <h2>User Guide</h2>
+        <p>
+        This user guide serves as an instruction manual for the Crazy Kitchen application.
+        </p>
 
-    <br>
-    <br>
+    <h2>Features</h2>
+        <p>
 
-    Note: Logo was created and not taken from the web.
-
-
-    </p>
+        </p>
 
     </div>
     <div class="payment">
